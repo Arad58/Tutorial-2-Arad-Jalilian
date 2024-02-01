a23ba63db506f6488c04ea0537ae72c84f5f2a2c
Arad Jalilian <B00163418@mytudublin.ie>
2024-02-01 17:28:37 +0000

    Create Hyperlinks.php

---
+++ Hyperlinks.php
@@ -0,0 +1,8 @@
+<!-- Makes hyperlinks for the user to see -->
+
+<ul class="nav navbar-nav">
+    <li class="active"><a href="/">Home</a></li>
+    <li><a href="/pets_new.php">Post</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#About">About</a></li>
+</ul>
